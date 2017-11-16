fa3fca1a2dddb0fd285933a52b76989a776a7676
Jean Feng <jean.feng@gmail.com>
2017-11-16 15:43:47 -0800

    rename some functions. fish one is also here now

--- gestalt/read_seq_data.py
+++ gestalt/read_seq_data.py
@@ -22,7 +22,6 @@ def parse_args():
         help='Collapsed reads file: format 7B')
     return parser.parse_args()
 
-
 def process_event_format7B(event_str: str, min_target: int, max_target: int):
     """
     Takes a single event string and creates an event object
@@ -42,45 +41,29 @@ def process_event_format7B(event_str: str, min_target: int, max_target: int):
     else:
         raise ValueError("Unrecognized event: %s" % event_str)
 
-
 def process_barcode_format7B(target_str_list: List[str], cell_type_tree: CellTypeTree):
     """
-    Converts 7B format barcode to python repr barcode
+    Converts new format barcode to python repr barcode
 
     @param target_str_list: targets with events encoded in 7B format
     @param cell_type_tree: the cell type associated with this barcode
     @return barcode in event-encoded format
     """
-    # Create a list of event strings for each target
-    target_evt_strs = [
-        target_str.split("&") for i, target_str in enumerate(target_str_list)
-    ]
-    # Filter the list of event strings so that we have no NO_EVENT_STRS
-    target_evt_strs = [[
-        evt for evt in target_evts if evt not in NO_EVENT_STRS
-    ] for target_evts in target_evt_strs]
-    # Create unique events
-    uniq_evt_strs = list(set([
-        evt_str for targ_list in target_evt_strs for evt_str in targ_list
-    ]))
+    # Find list of targets for each event
+    evt_target_dict = {}
+    for targ_idx, targ_str in enumerate(target_str_list):
+        target_evt_strs = targ_str.split("&")
+        for evt_str in target_evt_strs:
+            if evt_str not in evt_target_dict:
+                evt_target_dict[evt_str] = (targ_idx, targ_idx)
+            else:
+                min_targ, max_targ = evt_target_dict[evt_str]
+                evt_target_dict[evt_str] = (min(min_targ, targ_idx), max(max_targ, targ_idx))
     events = [
-        process_event_format7B(event_str) for event_str in uniq_evt_strs
+        process_event_format7B(event_str, min_targ, max_targ) for event_str, (min_targ, max_targ) in evt_target_dict.items() if event_str not in NO_EVENT_STRS
     ]
-    # For each target, associate list of events
-    event_str_dict = {
-        event_str: i for i, event_str in enumerate(uniq_evt_strs)
-    }
-    target_evts = [
-        [event_str_dict[evt_str] for evt_str in target_evts]
-        for target_evts in target_evt_strs
-    ]
-    # Associate each event with the matching targets
-    for target_idx, evts in enumerate(target_evts):
-        for evt in evts:
-            evt_targets = events[evt].add_target(target_idx)
 
-    return BarcodeEvents(target_evts, events, cell_type_tree)
-
+    return CellRead(BarcodeEvents(events), cell_type_tree)
 
 def parse_reads_file_format7B(file_name,
                               target_hdr_fmt="target%d",
@@ -125,30 +108,6 @@ def parse_reads_file_format7B(file_name,
         organ_dict[cell_type.cell_type] = organ_str
     return CellReads(all_barcodes, organ_dict)
 
-def process_barcode_newformat(target_str_list: List[str], cell_type_tree: CellTypeTree):
-    """
-    Converts new format barcode to python repr barcode
-
-    @param target_str_list: targets with events encoded in 7B format
-    @param cell_type_tree: the cell type associated with this barcode
-    @return barcode in event-encoded format
-    """
-    # Find list of targets for each event
-    evt_target_dict = {}
-    for targ_idx, targ_str in enumerate(target_str_list):
-        target_evt_strs = targ_str.split("&")
-        for evt_str in target_evt_strs:
-            if evt_str not in evt_target_dict:
-                evt_target_dict[evt_str] = (targ_idx, targ_idx)
-            else:
-                min_targ, max_targ = evt_target_dict[evt_str]
-                evt_target_dict[evt_str] = (min(min_targ, targ_idx), max(max_targ, targ_idx))
-    events = [
-        process_event_format7B(event_str, min_targ, max_targ) for event_str, (min_targ, max_targ) in evt_target_dict.items() if event_str not in NO_EVENT_STRS
-    ]
-
-    return CellRead(BarcodeEvents(events), cell_type_tree)
-
 def parse_reads_file_newformat(file_name,
                             organ_data_idx=1,
                             target_data_idx=2,
@@ -179,7 +138,7 @@ def parse_reads_file_newformat(file_name,
                 # Now create barcode representation
                 barcode_data = row[target_data_idx].split("_")
                 assert(len(barcode_data) == NUM_BARCODE_V7_TARGETS)
-                barcode_events = process_barcode_newformat(
+                barcode_events = process_barcode_format7B(
                     barcode_data,
                     cell_type_tree)
                 all_barcodes.append(barcode_events)
