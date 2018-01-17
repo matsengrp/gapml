259368910e32abb0389b85734c1a75fe8c975e69
Jean Feng <jean.feng@gmail.com>
2018-01-17 14:53:21 -0800

    update comments

--- gestalt/transition_matrix.py
+++ gestalt/transition_matrix.py
@@ -1,14 +1,12 @@
 from typing import Dict
-import scipy.sparse as sparse
 import numpy as np
 
 class TransitionMatrixWrapper:
     def __init__(self, matrix_dict: Dict[str, Dict[str, float]]):
         """
-        Create sparse matrix matrix given the dictionary representation
-        Assume each key in the dictionary corresponds to a matrix row
-
-        TODO: decide if we should still call this "sparse matrix"
+        Create transition matrix given the dictionary representation.
+        Assume each key in the dictionary corresponds to a possible
+        target tract representation state.
         """
         # Number each key
         self.key_list = []
@@ -25,26 +23,8 @@ class TransitionMatrixWrapper:
         for i, key in enumerate(self.key_list):
             for to_key, val in matrix_dict[key].items():
                 self.matrix[i, self.key_dict[to_key]] = val
-        #coo_rows = []
-        #coo_cols = []
-        #coo_vals = []
-        #for i, key in enumerate(self.key_list):
-        #    for to_key, val in matrix_dict[key].items():
-        #        coo_rows.append(i)
-        #        coo_cols.append(self.key_dict[to_key])
-        #        coo_vals.append(val)
 
-        #coo_rows.append(0)
-        #coo_cols.append(0)
-        #coo_vals.append(1)
-
-        #coo_rows.append(1)
-        #coo_cols.append(1)
-        #coo_vals.append(1)
-
-        #sparse_matrix = sparse.coo_matrix((coo_vals, (coo_rows, coo_cols)))
-        #self.matrix = sparse_matrix.tocsr()
-        #self.diag_mat, self.right_eig_mat = sparse.linalg.eigs(self.matrix, k=self.num_states)
+        # Store the matrix diagonlization decomposition
         self.diag_mat, self.right_eig_mat = np.linalg.eig(self.matrix)
         self.right_eig_mat_inv = np.linalg.inv(self.right_eig_mat)
 
