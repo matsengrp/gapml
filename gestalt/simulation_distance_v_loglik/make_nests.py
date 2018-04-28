f3b7d45fbce3f93932295a64f8d9aac1576c12ec
Jean Feng <jean.feng@gmail.com>
2018-04-28 13:13:43 -0700

    add simulation scripts

---
+++ gestalt/simulation_distance_v_loglik/make_nests.py
@@ -0,0 +1,33 @@
+import os
+from os.path import join
+from nestly import Nest
+import numpy as np
+
+nest = Nest()
+
+nest.add(
+    'max_leaves',
+    [50],
+    label_func=lambda c: "maxleaves%d" % c)
+
+nest.add(
+    'time',
+    [3.2],
+    label_func=lambda c: "time%d" % int(c * 10))
+
+nest.add(
+    'variance',
+    [0.005, 0.004, 0.003, 0.002, 0.001],
+    label_func=lambda c: 'var%d' % (c * 1000))
+
+nest.add(
+    'model_seed',
+    range(32,33),
+    label_func=lambda c: 'model%d' % c)
+
+nest.add(
+    'data_seed',
+    lambda c: [c['model_seed']],
+    label_func=lambda c: 'data%d' % c)
+
+nest.build('_output')
