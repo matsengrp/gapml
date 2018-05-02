69d2a33fb9375554e6dd105754b3a9a5497be581
Jean Feng <jean.feng@gmail.com>
2018-05-02 16:39:01 -0700

    add scons for simulation multifurc

---
+++ gestalt/simulation_multifurc/make_nests.py
@@ -0,0 +1,39 @@
+import os
+from os.path import join
+from nestly import Nest
+import numpy as np
+
+nest = Nest()
+
+nest.add(
+    'max_leaves',
+    [80],
+    label_func=lambda c: "maxleaves%d" % c)
+
+nest.add(
+    'min_leaves',
+    [40],
+    label_func=lambda c: "minleaves%d" % c)
+
+nest.add(
+    'time',
+    [4.5],
+    label_func=lambda c: "time%d" % int(c * 10))
+
+nest.add(
+    'model_seed',
+    range(200,210),
+    label_func=lambda c: 'model%d' % c)
+
+nest.add(
+    'data_seed',
+    lambda c: [c['model_seed']],
+    label_func=lambda c: 'data%d' % c)
+
+
+nest.add(
+    'variance',
+    [0.03**2, 0.01**2],
+    label_func=lambda c: 'var%.6f' % c)
+
+nest.build('_output')
---
+++ gestalt/simulation_multifurc/script.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+cd /home/jfeng2/gestaltamania/gestalt/
+
+OVERWRITE="0"
+
+FILE="simulation_multifurc/_output/{OUTDIR}/estimator_multifurc.pkl"
+echo $FILE
+# Check if file exists already or overwrite is allowed
+if [ ! -f "$FILE" ] || [ "$OVERWRITE" = "1" ]; then
+    srun -p restart,matsen_e,campus --cpus-per-task 3 python simulate_estimators.py \
+        --model-seed {model_seed} \
+        --data-seed {data_seed} \
+        --time {time} \
+	--min-leaves {min_leaves} \
+	--sampling-rate 0.8 \
+	--max-leaves {max_leaves} \
+        --variance-target-lam {variance} \
+	--max-iters 3000 \
+	--use-parsimony \
+        --num-jumbles 1 \
+        --out-folder simulation_multifurc/_output/{OUTDIR}
+else
+    echo "File exists already"
+fi
