fix up target status transition hazard calculation

gestalt/clt_likelihood_model.py
@@ -110,6 +110,7 @@ class CLTLikelihoodModel:
             self.branch_lens = self._create_branch_lens()
 
         self._create_hazard_node_for_simulation()
+        self.target_tract_hazards, self.target_tract_dict = self._create_all_target_tract_hazards()
 
         self.adam_opt = tf.train.AdamOptimizer(learning_rate=0.005)
 
@@ -378,25 +379,23 @@ class CLTLikelihoodModel:
                 self.long_status_ph[:,0],
                 self.long_status_ph[:,1])
 
-    def _create_target_tract_hazards(self, target_tracts: List[TargetTract]):
+    def _create_all_target_tract_hazards(self):
         """
-        @param target_tracts: a list of target tracts that we want to create hazard nodes for
-
-        @return Dict[TargetTract, tensorflow node calculating hazard of that target tract]
+        @return Tuple with:
+            tensorflow tensor with hazards for all possible target tracts
+            dictionary mapping all possible target tracts to their index in the tensor
         """
-        min_targets = tf.constant([tt.min_target for tt in target_tracts], dtype=tf.int32)
-        max_targets = tf.constant([tt.max_target for tt in target_tracts], dtype=tf.int32)
-        long_left_statuses = tf.constant([tt.is_left_long for tt in target_tracts], dtype=tf.float64)
-        long_right_statuses = tf.constant([tt.is_right_long for tt in target_tracts], dtype=tf.float64)
+        target_status_all_active = TargetStatus()
+        all_target_tracts = target_status_all_active.get_possible_target_tracts(self.bcode_meta)
+        tt_dict = {tt: i for i, tt in enumerate(all_target_tracts)}
 
-        # Compute the hazard
-        hazard_target_tracts = self._create_hazard_target_tract(min_targets, max_targets, long_left_statuses, long_right_statuses)
+        min_targets = tf.constant([tt.min_target for tt in all_target_tracts], dtype=tf.int32)
+        max_targets = tf.constant([tt.max_target for tt in all_target_tracts], dtype=tf.int32)
+        long_left_statuses = tf.constant([tt.is_left_long for tt in all_target_tracts], dtype=tf.float64)
+        long_right_statuses = tf.constant([tt.is_right_long for tt in all_target_tracts], dtype=tf.float64)
 
-        # Now create the dictionary
-        hazard_tt_dict = {}
-        for i, tt in enumerate(target_tracts):
-            hazard_tt_dict[tt] = hazard_target_tracts[i]
-        return hazard_tt_dict
+        all_hazards = self._create_hazard_target_tract(min_targets, max_targets, long_left_statuses, long_right_statuses)
+        return all_hazards, tt_dict
 
     def _create_hazard_target_tract(self,
             min_target: Tensor,
@@ -422,76 +421,6 @@ class CLTLikelihoodModel:
         left_trim_prob = tf_common.ifelse(long_left_statuses, self.trim_long_probs[0], 1 - self.trim_long_probs[0])
         right_trim_prob = tf_common.ifelse(long_right_statuses, self.trim_long_probs[1], 1 - self.trim_long_probs[1])
         hazard = tf.exp(log_lambda_part + tf.log(left_trim_prob) + tf.log(right_trim_prob), name="hazard")
-        return hazard
-    
-    def _create_target_status_transition_hazards(self):
-        """
-        @return tuple of:
-            1. Dict[start TargetStatus, Dict[end TargetStatus, index of the (start,end) target status pair in the tensorflow tensor]]
-            2. tensorflow tensor calculating hazard of transitioning between the target statuses
-        """
-        deact_targ_starts = []
-        deact_targ_ends = []
-        target_status_transition_idxs = dict()
-        idx = 0
-        for start_targ_stat, end_targ_dict in self.targ_stat_transitions_dict.items():
-            target_status_transition_idxs[start_targ_stat] = dict()
-            for end_targ_stat, deact_tract in end_targ_dict.items():
-                deact_targ_starts.append(deact_tract.min_deact_target)
-                deact_targ_ends.append(deact_tract.max_deact_target)
-                target_status_transition_idxs[start_targ_stat][end_targ_stat] = idx
-                idx += 1
-    
-        t0 = tf.constant(deact_targ_starts, dtype=tf.int32)
-        t1 = tf.constant(deact_targ_ends, dtype=tf.int32)
-        hazards = self._create_hazard_deact(t0, t1)
-    
-        return target_status_transition_idxs, hazards
-
-    def _create_hazard_deact(self, t0: List[int], t1: List[int]):
-        """
-        Create the tensorflow node for the hazard of adding a new deact tract that deactivates targets t0 to t1
-        This is calculated by summing over the hazards of introducing all target tracts that match that deactivation range
-
-        @param t0: first target deactivated
-        @param t1: last target deactivated
-
-        @return tensorflow tensor where i-th value corresponds to i-th deact tract
-        """
-        left_short_lambda = tf.gather(self.target_lams, t0) * self.trim_short_probs[0]
-        right_short_lambda = tf.gather(self.target_lams, t1) * self.trim_short_probs[1]
-        left_long_lambda = tf.gather(self.target_lams, tf.minimum(t0 + 1, self.num_targets - 1)) * self.trim_long_probs[0]
-        right_long_lambda = tf.gather(self.target_lams, tf.maximum(t1 - 1, 0)) * self.trim_long_probs[1]
-
-        # one target deactivated
-        is_one = tf_common.equal_float(t1, t0)
-        lambda_one = left_short_lambda * self.trim_short_probs[1]
-
-        # two targets consecutive
-        is_two_consec = tf_common.equal_float(t0 + 1, t1)
-        lambda_two_consec = (left_short_lambda * self.trim_long_probs[1]
-                            + right_short_lambda * self.trim_long_probs[0]
-                            + left_short_lambda * right_short_lambda)
-
-        # three targets consecutive
-        is_three_consec = tf_common.equal_float(t0 + 2, t1)
-        lambda_three_consec = (left_short_lambda * right_long_lambda
-                            + left_long_lambda * right_short_lambda
-                            + left_long_lambda * self.trim_long_probs[1]
-                            + left_short_lambda * right_short_lambda)
-
-        is_four_or_more = tf_common.greater_equal_float(t1, t0 + 3)
-        lambda_four = (left_short_lambda * right_short_lambda
-                + left_short_lambda * right_long_lambda
-                + left_long_lambda * right_short_lambda
-                + left_long_lambda * right_long_lambda)
-
-        # add them all up
-        hazard = (lambda_four * is_four_or_more
-                + lambda_three_consec * is_three_consec
-                + lambda_two_consec * is_two_consec
-                + lambda_one * is_one)
-
         return hazard
 
     def _create_hazard_away_dict(self):
@@ -714,13 +643,15 @@ class CLTLikelihoodModel:
         """
         Create a tensorflow graph of the likelihood calculation
         """
+        # Dictionary for storing hazards between target statuses -- assuming all moves are possible
+        self.targ_stat_transition_hazards_dict = {
+            start_target_status: {} for start_target_status in self.targ_stat_transitions_dict.keys()}
+
         # Get the hazards for making the instantaneous transition matrix
         # Doing it all at once to speed up computation
-        self.target_status_transition_idxs, self.target_status_transition_hazards = self._create_target_status_transition_hazards()
         self.hazard_away_dict = self._create_hazard_away_dict()
         singletons = CLTLikelihoodModel.get_all_singletons(self.topology)
         target_tracts = [sg.get_target_tract() for sg in singletons]
-        self.hazard_target_tract_dict = self._create_target_tract_hazards(target_tracts)
 
         # Get all the conditional probabilities of the trims
         # Doing it all at once to speed up computation
@@ -870,9 +801,9 @@ class CLTLikelihoodModel:
         """
         # Get the target tracts of the singletons -- this is important
         # since the transition matrix excludes the impossible target tracts
-        special_deact_tracts = {
-                (sgwc.min_deact_target, sgwc.max_deact_target): sgwc
-                for sgwc in transition_wrapper.anc_state.get_singleton_wcs()}
+        special_tts = set([
+                sgwc.get_singleton().get_target_tract()
+                for sgwc in transition_wrapper.anc_state.get_singleton_wcs()])
 
         possible_states = set(transition_wrapper.states)
         impossible_key = transition_wrapper.num_likely_states
@@ -885,21 +816,30 @@ class CLTLikelihoodModel:
             # Hazard of staying is negative of hazard away
             index_vals.append([[start_key, start_key], -haz_away])
 
-            idxs_for_start_state = self.target_status_transition_idxs[start_state]
-            all_end_states = set(idxs_for_start_state.keys())
+            all_end_states = set(self.targ_stat_transitions_dict[start_state].keys())
             possible_end_states = all_end_states.intersection(possible_states)
             haz_to_possible = 0
             for end_state in possible_end_states:
                 # Figure out if this is a special transition involving only a particular target tract
                 # Or if it a general any-target-tract transition
-                transition_deact_tract = self.targ_stat_transitions_dict[start_state][end_state]
-                deact_target_range = (transition_deact_tract.min_deact_target, transition_deact_tract.max_deact_target)
-                if deact_target_range in special_deact_tracts:
-                    target_tract = special_deact_tracts[deact_target_range].get_singleton().get_target_tract()
-                    hazard = self.hazard_target_tract_dict[target_tract]
+                target_tracts_for_transition = self.targ_stat_transitions_dict[start_state][end_state]
+                matching_tts = special_tts.intersection(target_tracts_for_transition)
+                if matching_tts:
+                    assert len(matching_tts) == 1
+                    matching_tt = list(matching_tts)[0]
+                    hazard = self.target_tract_hazards[self.target_tract_dict[matching_tt]]
                 else:
-                    end_haz_idx = idxs_for_start_state[end_state]
-                    hazard = self.target_status_transition_hazards[end_haz_idx]
+                    # if we already calculated the hazard of the transition between these target statuses,
+                    # use the same node
+                    if end_state in self.targ_stat_transition_hazards_dict[start_state]:
+                        hazard = self.targ_stat_transition_hazards_dict[start_state][end_state]
+                        index_vals.append([[start_key, end_key], hazard])
+                    else:
+                        hazard = tf.add_n(
+                            [self.target_tract_hazards[self.target_tract_dict[tt]] for tt in target_tracts_for_transition])
+
+                        # Store this hazard if we need it in the future
+                        self.targ_stat_transition_hazards_dict[start_state][end_state] = hazard
 
                 end_key = transition_wrapper.key_dict[end_state]
                 haz_to_possible += hazard
@@ -908,10 +848,11 @@ class CLTLikelihoodModel:
             # Hazard to unlikely state is hazard away minus hazard to likely states
             index_vals.append([[start_key, impossible_key], haz_away - haz_to_possible])
 
+        matrix_len = transition_wrapper.num_likely_states + 1
         q_matrix = tf_common.scatter_nd(
-                index_vals,
-                output_shape=[transition_wrapper.num_likely_states + 1, transition_wrapper.num_likely_states + 1],
-                name="top.q_matrix")
+            index_vals,
+            output_shape=[matrix_len, matrix_len],
+            name="top.q_matrix")
         return q_matrix
 
     def _create_trim_prob_matrix(self, child_transition_wrapper: TransitionWrapper):

gestalt/tests/test_clt_likelihood_model.py
@@ -6,17 +6,19 @@ import numpy as np
 from clt_likelihood_model import CLTLikelihoodModel
 from cell_lineage_tree import CellLineageTree
 from barcode_metadata import BarcodeMetadata
-from approximator import TransitionGraph, TransitionToNode
-from transition_matrix import TransitionMatrixWrapper
-from indel_sets import *
+from indel_sets import TargetTract
+from allele_events import AlleleEvents
+from target_status import TargetStatus, TargetDeactTract
 
 class LikelihoodModelTestCase(unittest.TestCase):
     def setUp(self):
-        topology = CellLineageTree()
+        self.num_targets = 10
+        #topology = CellLineageTree(allele_events_list = [AlleleEvents(num_targets=self.num_targets)])
+        #topology.add_feature("node_id", 0)
         bcode_metadata = BarcodeMetadata()
         self.sess = tf.InteractiveSession()
         self.mdl = CLTLikelihoodModel(
-                topology,
+                None,
                 bcode_metadata,
                 self.sess,
                 target_lams = 0.1 + np.arange(bcode_metadata.n_targets))
@@ -24,17 +26,6 @@ class LikelihoodModelTestCase(unittest.TestCase):
         tf.global_variables_initializer().run()
         self.target_lams = self.mdl.target_lams.eval()
         self.trim_long_probs = self.mdl.trim_long_probs.eval()
-
-    def test_matching_singletons(self):
-        anc_state = AncState(set([Wildcard(0,1), SingletonWC(40,3, 2,2,2,3)]))
-        tts = (TargetTract(2,2,2,2),)
-        matches = CLTLikelihoodModel.get_matching_singletons(anc_state, tts)
-        self.assertEqual(len(matches), 0)
-
-        anc_state = AncState(set([Wildcard(0,1), SingletonWC(40,3, 2,2,2,3)]))
-        tts = (TargetTract(0,0,1,1), TargetTract(2,2,2,3),)
-        matches = CLTLikelihoodModel.get_matching_singletons(anc_state, tts)
-        self.assertEqual(matches, [Singleton(40,3, 2,2,2,3)])
 
     def test_get_hazard(self):
         tt = TargetTract(2,2,2,2)
@@ -47,100 +38,109 @@ class LikelihoodModelTestCase(unittest.TestCase):
         self.assertTrue(np.isclose(hazard,
             self.target_lams[0] * self.target_lams[2] * (1 - self.trim_long_probs[0]) * (1 - self.trim_long_probs[1])))
 
+    def _get_transition_target_status_hazard(
+            self,
+            target_stat_start,
+            target_stat_end):
+        hazard = self.sess.run(
+            self.mdl.target_status_transition_hazards[
+                self.mdl.target_status_transition_idxs[target_stat_start][target_stat_end]])
+        return hazard
+
     def test_create_hazard(self):
-        tt_evts = [
-                TargetTract(2,2,2,2),
-                TargetTract(0,0,2,2)]
-        hazard_node = self.mdl._create_hazard_nodes(tt_evts)
-        hazards = self.sess.run(hazard_node)
-
-        self.assertTrue(np.isclose(hazards[0],
-            self.target_lams[2] * (1 - self.trim_long_probs[0]) * (1 - self.trim_long_probs[1])))
-        self.assertTrue(np.isclose(hazards[1],
-            self.target_lams[0] * self.target_lams[2] * (1 - self.trim_long_probs[0]) * (1 - self.trim_long_probs[1])))
-
-    def test_create_hazard_away(self):
-        tts_list = [
-                (TargetTract(1,1,9,9),),
-                (TargetTract(0,1,7,8),),
-                (TargetTract(0,1,1,1), TargetTract(3,4,8,9)),
-                (TargetTract(0,1,1,1), TargetTract(4,4,8,9))]
-
-        hazard_away_node = self.mdl._create_hazard_away_nodes(tts_list)
-        hazard_aways = self.sess.run(hazard_away_node)
-
-        self.assertTrue(np.isclose(hazard_aways[0],
-            self.target_lams[0] * (1 - self.trim_long_probs[0]) * (1 - self.trim_long_probs[1])))
-
-        self.assertTrue(np.isclose(hazard_aways[1],
-            self.target_lams[9] * (1 - self.trim_long_probs[0]) * (1 - self.trim_long_probs[1])))
-
-        self.assertTrue(np.isclose(hazard_aways[2],
-            self.target_lams[2] * (1 - self.trim_long_probs[0]) * (1 - self.trim_long_probs[1])))
-
-        self.assertTrue(np.isclose(hazard_aways[3],
-            self.target_lams[2] * (1 - self.trim_long_probs[0])
-            + self.target_lams[2] * (1 - self.trim_long_probs[0]) * self.target_lams[3] * (1 - self.trim_long_probs[1])
-            + self.target_lams[3] * (1 - self.trim_long_probs[1])))
-
-    def test_list_target_tracts(self):
-        active_any_targs = [0,1]
-        any_tts = CLTLikelihoodModel.get_possible_target_tracts(active_any_targs)
-        self.assertEqual(any_tts, set([
-            TargetTract(0,0,0,0),
-            TargetTract(0,0,0,1),
-            TargetTract(0,0,1,1),
-            TargetTract(0,1,1,1),
-            TargetTract(1,1,1,1)]))
-
-        active_any_targs = [0,5]
-        any_tts = CLTLikelihoodModel.get_possible_target_tracts(active_any_targs)
-        self.assertEqual(any_tts, set([
-            TargetTract(0,0,0,0),
-            TargetTract(0,0,5,5),
-            TargetTract(5,5,5,5)]))
-
-    def test_create_transition_matrix(self):
-        key_list = [(), (TargetTract(1,1,1,1),), (TargetTract(1,1,1,2),), (TargetTract(1,1,1,1), TargetTract(2,2,2,2))]
-        mat_wrap = TransitionMatrixWrapper({
-            (): {
-                key_list[1]: TargetTract(1,1,1,1),
-                key_list[2]: TargetTract(1,1,1,2),
-                },
-            key_list[1]: {key_list[3]: TargetTract(2,2,2,2)},
-            key_list[2]: {},
-            key_list[3]: {}
-        }, key_list)
-        hazard_dict, _, _ = self.mdl._create_hazard_dict([mat_wrap])
-        hazard_away_dict, _ = self.mdl._create_hazard_away_dict([mat_wrap])
-        q_mat_node = self.mdl._create_transition_matrix(mat_wrap, hazard_dict, hazard_away_dict)
-        transition_matrix = self.sess.run(q_mat_node)
-
-        hazard_away_node = self.mdl._create_hazard_away_nodes(key_list)
-        hazard_aways = self.sess.run(hazard_away_node)
-
+        target_stat_start = TargetStatus(TargetDeactTract(1,9))
+        target_stat_end = TargetStatus(TargetDeactTract(0,9))
+        hazard = self._get_transition_target_status_hazard(
+                target_stat_start,
+                target_stat_end)
+        print(hazard)
+        print(self.target_lams[0] * (1 - self.trim_long_probs[0]))
         self.assertTrue(np.isclose(
-            transition_matrix[0][1],
-            self.target_lams[1] * (1 - self.trim_long_probs[0]) * (1 - self.trim_long_probs[1])))
-        self.assertTrue(np.isclose(
-            transition_matrix[0][2],
-            self.target_lams[1] * (1 - self.trim_long_probs[0]) * self.trim_long_probs[1]))
-        self.assertTrue(np.isclose(transition_matrix[0][3], 0))
-        self.assertTrue(np.isclose(
-            transition_matrix[0][4], hazard_aways[0] - self.target_lams[1] * (1 - self.trim_long_probs[0])))
-        self.assertTrue(np.isclose(
-            transition_matrix[0][0], -hazard_aways[0]))
-        self.assertTrue(np.isclose(
-            transition_matrix[1][0], 0))
-        self.assertTrue(np.isclose(
-            transition_matrix[1][1], -hazard_aways[1]))
-        self.assertTrue(np.isclose(
-            transition_matrix[1][2], 0))
-        self.assertTrue(np.isclose(
-            transition_matrix[1][3],
-            self.target_lams[2] * (1 - self.trim_long_probs[0]) * (1 - self.trim_long_probs[1])))
-        self.assertTrue(np.isclose(
-            transition_matrix[3][4], hazard_aways[3]))
-        # Unlikely row is all zeros
-        self.assertTrue(np.isclose(
-            np.max(np.abs(transition_matrix[4])), 0))
+            hazard,
+            self.target_lams[0] * (1 - self.trim_long_probs[0])))
+
+#    def test_create_hazard_away(self):
+#        tts_list = [
+#                (TargetTract(1,1,9,9),),
+#                (TargetTract(0,1,7,8),),
+#                (TargetTract(0,1,1,1), TargetTract(3,4,8,9)),
+#                (TargetTract(0,1,1,1), TargetTract(4,4,8,9))]
+#
+#        hazard_away_node = self.mdl._create_hazard_away_nodes(tts_list)
+#        hazard_aways = self.sess.run(hazard_away_node)
+#
+#        self.assertTrue(np.isclose(hazard_aways[0],
+#            self.target_lams[0] * (1 - self.trim_long_probs[0]) * (1 - self.trim_long_probs[1])))
+#
+#        self.assertTrue(np.isclose(hazard_aways[1],
+#            self.target_lams[9] * (1 - self.trim_long_probs[0]) * (1 - self.trim_long_probs[1])))
+#
+#        self.assertTrue(np.isclose(hazard_aways[2],
+#            self.target_lams[2] * (1 - self.trim_long_probs[0]) * (1 - self.trim_long_probs[1])))
+#
+#        self.assertTrue(np.isclose(hazard_aways[3],
+#            self.target_lams[2] * (1 - self.trim_long_probs[0])
+#            + self.target_lams[2] * (1 - self.trim_long_probs[0]) * self.target_lams[3] * (1 - self.trim_long_probs[1])
+#            + self.target_lams[3] * (1 - self.trim_long_probs[1])))
+#
+#    def test_list_target_tracts(self):
+#        active_any_targs = [0,1]
+#        any_tts = CLTLikelihoodModel.get_possible_target_tracts(active_any_targs)
+#        self.assertEqual(any_tts, set([
+#            TargetTract(0,0,0,0),
+#            TargetTract(0,0,0,1),
+#            TargetTract(0,0,1,1),
+#            TargetTract(0,1,1,1),
+#            TargetTract(1,1,1,1)]))
+#
+#        active_any_targs = [0,5]
+#        any_tts = CLTLikelihoodModel.get_possible_target_tracts(active_any_targs)
+#        self.assertEqual(any_tts, set([
+#            TargetTract(0,0,0,0),
+#            TargetTract(0,0,5,5),
+#            TargetTract(5,5,5,5)]))
+#
+#    def test_create_transition_matrix(self):
+#        key_list = [(), (TargetTract(1,1,1,1),), (TargetTract(1,1,1,2),), (TargetTract(1,1,1,1), TargetTract(2,2,2,2))]
+#        mat_wrap = TransitionMatrixWrapper({
+#            (): {
+#                key_list[1]: TargetTract(1,1,1,1),
+#                key_list[2]: TargetTract(1,1,1,2),
+#                },
+#            key_list[1]: {key_list[3]: TargetTract(2,2,2,2)},
+#            key_list[2]: {},
+#            key_list[3]: {}
+#        }, key_list)
+#        hazard_dict, _, _ = self.mdl._create_hazard_dict([mat_wrap])
+#        hazard_away_dict, _ = self.mdl._create_hazard_away_dict([mat_wrap])
+#        q_mat_node = self.mdl._create_transition_matrix(mat_wrap, hazard_dict, hazard_away_dict)
+#        transition_matrix = self.sess.run(q_mat_node)
+#
+#        hazard_away_node = self.mdl._create_hazard_away_nodes(key_list)
+#        hazard_aways = self.sess.run(hazard_away_node)
+#
+#        self.assertTrue(np.isclose(
+#            transition_matrix[0][1],
+#            self.target_lams[1] * (1 - self.trim_long_probs[0]) * (1 - self.trim_long_probs[1])))
+#        self.assertTrue(np.isclose(
+#            transition_matrix[0][2],
+#            self.target_lams[1] * (1 - self.trim_long_probs[0]) * self.trim_long_probs[1]))
+#        self.assertTrue(np.isclose(transition_matrix[0][3], 0))
+#        self.assertTrue(np.isclose(
+#            transition_matrix[0][4], hazard_aways[0] - self.target_lams[1] * (1 - self.trim_long_probs[0])))
+#        self.assertTrue(np.isclose(
+#            transition_matrix[0][0], -hazard_aways[0]))
+#        self.assertTrue(np.isclose(
+#            transition_matrix[1][0], 0))
+#        self.assertTrue(np.isclose(
+#            transition_matrix[1][1], -hazard_aways[1]))
+#        self.assertTrue(np.isclose(
+#            transition_matrix[1][2], 0))
+#        self.assertTrue(np.isclose(
+#            transition_matrix[1][3],
+#            self.target_lams[2] * (1 - self.trim_long_probs[0]) * (1 - self.trim_long_probs[1])))
+#        self.assertTrue(np.isclose(
+#            transition_matrix[3][4], hazard_aways[3]))
+#        # Unlikely row is all zeros
+#        self.assertTrue(np.isclose(
+#            np.max(np.abs(transition_matrix[4])), 0))