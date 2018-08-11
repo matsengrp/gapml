2e5bb8f50801d6e71239ff073303e5c5549a0674
Jean Feng <jean.feng@gmail.com>
2018-08-10 18:19:21 -0700

    fix up test

--- gestalt/tests/test_clt_likelihood_calc.py
+++ gestalt/tests/test_clt_likelihood_calc.py
@@ -13,6 +13,7 @@ from barcode_metadata import BarcodeMetadata
 from collapsed_tree import collapse_zero_lens
 from transition_wrapper_maker import TransitionWrapperMaker
 from target_status import TargetStatus, TargetDeactTract
+from optim_settings import KnownModelParams
 
 class LikelihoodCalculationTestCase(unittest.TestCase):
     def setUp(self):
@@ -21,6 +22,7 @@ class LikelihoodCalculationTestCase(unittest.TestCase):
             cut_site = 3,
             crucial_pos_len = [3,3])
         self.num_targets = self.bcode_metadata.n_targets
+        self.known_params = KnownModelParams(tot_time=True)
 
     def _create_bifurc_model(
             self,
@@ -54,13 +56,14 @@ class LikelihoodCalculationTestCase(unittest.TestCase):
                 branch_len_inners = np.array(branch_lens),
                 branch_len_offsets_proportion = 1e-20 * np.ones(num_nodes), # dummy!!!
                 target_lams = target_lams,
+                known_params = self.known_params,
                 trim_long_probs = 0.1 * np.ones(2),
                 trim_zero_probs = 0.5 * np.ones(2),
                 trim_short_poissons = np.ones(2),
                 trim_long_poissons = np.ones(2),
                 insert_zero_prob = 0.5,
                 insert_poisson = 2,
-                double_cut_weight = double_cut_weight,
+                double_cut_weight = [double_cut_weight],
                 tot_time = tot_time)
         tf.global_variables_initializer().run()
         return model
@@ -87,13 +90,14 @@ class LikelihoodCalculationTestCase(unittest.TestCase):
                 branch_len_inners = br_len_inners,
                 branch_len_offsets_proportion = br_len_offsets/(br_len_inners + 1e-10),
                 target_lams = target_lams,
+                known_params = self.known_params,
                 trim_long_probs = 0.1 * np.ones(2),
                 trim_zero_probs = 0.5 * np.ones(2),
                 trim_short_poissons = np.ones(2),
                 trim_long_poissons = np.ones(2),
                 insert_zero_prob = 0.5,
                 insert_poisson = 2,
-                double_cut_weight = double_cut_weight,
+                double_cut_weight = [double_cut_weight],
                 tot_time = tot_time)
         tf.global_variables_initializer().run()
         return model, target_lams
