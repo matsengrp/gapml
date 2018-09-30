bbfa5da753df6028ccde6a02ab519a2f2bd54aec
Jean Feng <jean.feng@gmail.com>
2018-09-29 17:44:30 -0700

    clean up hyperparam tuning and tree splitting code

--- gestalt/hyperparam_tuner.py
+++ gestalt/hyperparam_tuner.py
@@ -20,9 +20,6 @@ class PenaltyScorerResult:
             score: float,
             fit_results: List[LikelihoodScorerResult]):
         """
-        @param branch_pen_param: the penalty parameter to make branch lengths more similar
-        @param model_params_dict: an example of the final fitted parameters when we did
-                        train/validation split/kfold CV. (this is used mostly as a warm start)
         @param score: assumed to be higher the better
         """
         self.score = score
@@ -80,7 +77,7 @@ def tune(
             args,
             fit_params,
             create_kfold_barcode_trees,
-            _get_many_bcode_stability_score,
+            _get_many_bcode_hyperparam_score,
             assessor)
     else:
         fit_params.pop('branch_len_inners', None)
@@ -92,7 +89,7 @@ def tune(
             args,
             fit_params,
             create_kfold_trees,
-            _get_one_bcode_stability_score,
+            _get_one_bcode_hyperparam_score,
             assessor)
 
 
@@ -102,7 +99,7 @@ def _tune_hyperparams(
         args,
         fit_params: Dict,
         kfold_fnc,
-        stability_score_fnc,
+        hyperparam_score_fnc,
         assessor: ModelAssessor = None,
         conv_thres: float = 1e-5):
     """
@@ -116,17 +113,16 @@ def _tune_hyperparams(
     """
     # First split the barcode into kfold groups
     n_splits = args.num_penalty_tune_splits if bcode_meta.num_barcodes == 1 else min(args.num_penalty_tune_splits, bcode_meta.num_barcodes)
-    logging.info("hyperparam tuning %d splits", n_splits)
-    all_tree_splits = kfold_fnc(
-            tree,
-            bcode_meta,
-            n_splits)
+    logging.info("Hyperparam tuning %d splits", n_splits)
+
+    # Make all the tree splits, but we will only fit a random number of them
+    all_tree_splits = kfold_fnc(tree, bcode_meta, n_splits)
     random.shuffle(all_tree_splits)
     tree_splits = all_tree_splits[:args.max_fit_splits]
 
     trans_wrap_makers = [TransitionWrapperMaker(
-            tree_split.tree,
-            tree_split.bcode_meta,
+            tree_split.train_clt,
+            tree_split.train_bcode_meta,
             args.max_extra_steps,
             args.max_sum_states) for tree_split in tree_splits]
 
@@ -144,12 +140,10 @@ def _tune_hyperparams(
             fit_param_list.append(new_fit_params)
 
     # Actually fit the trees using the kfold barcodes
-    # TODO: if one of the penalty params fails, then remove it from the subsequent
-    # kfold runs
     worker_list = [LikelihoodScorer(
         get_randint(),
-        tree_split.tree,
-        tree_split.bcode_meta,
+        tree_split.train_clt,
+        tree_split.train_bcode_meta,
         args.max_iters,
         args.num_inits,
         transition_wrap_maker,
@@ -181,15 +175,21 @@ def _tune_hyperparams(
                 fit_param['branch_pen_param'],
                 fit_param['target_lam_pen_param'])
         res_folds = [train_res[idx] for train_res in train_results]
-        stability_score = stability_score_fnc(res_folds, tree_splits, tree)
+        hyperparam_score = hyperparam_score_fnc(
+            res_folds,
+            tree_splits,
+            args.max_extra_steps,
+            args.max_sum_states,
+            args.scratch_dir,
+            args.num_processes)
 
         # Create our summary of tuning
         tune_result = PenaltyScorerResult(
-            stability_score,
+            hyperparam_score,
             res_folds)
         tune_results.append(tune_result)
         logging.info(
-                "Pen param branch %f, target_lam %f, stability score %s",
+                "Pen param branch %f, target_lam %f, hyperparam score %s",
                 branch_pen_param,
                 target_lam_pen_param,
                 tune_result.score)
@@ -200,128 +200,41 @@ def _tune_hyperparams(
                 tune_results)
 
 
-def _get_many_bcode_stability_score(
+def _get_many_bcode_hyperparam_score(
         pen_param_results: List[LikelihoodScorerResult],
         tree_splits: List[TreeDataSplit],
-        orig_tree: CellLineageTree,
-        max_num_leaves: int = None):
+        max_extra_steps: int,
+        max_sum_states: int,
+        scratch_dir: str,
+        num_processes: int):
     """
-    Compare via the validation log likelihood
+    @return score = the validation log likelihood
     """
-    is_stable = True
     for pen_param_res in pen_param_results:
         if pen_param_res is None:
-            is_stable = False
-            break
+            # This pen param setting is not stable
+            return -np.inf
 
-    if is_stable:
-        worker_list = []
-        for pen_param_res, tree_split in zip(pen_param_results, tree_splits):
-            fit_params = pen_param_res.get_fit_params()
-            all_known_params = KnownModelParams(
-                target_lams=True,
-                tot_time=True,
-                indel_params=True)
-            transition_wrap_maker = TransitionWrapperMaker(
-                tree_split.val_clt,
-                tree_split.val_bcode_meta,
-                100,
-                1000)
-            scorer = LikelihoodScorer(
-                1, # seed
-                tree_split.val_clt,
-                tree_split.val_bcode_meta,
-                max_iters=0, #50000,
-                num_inits=1,
-                transition_wrap_maker=transition_wrap_maker,
-                fit_param_list=[fit_params],
-                known_params=all_known_params,
-                scratch_dir="_output/scratch")
-            worker_list.append(scorer)
-
-        job_manager = SubprocessManager(
-                worker_list,
-                None,
-                "_output/scratch",
-                10)
-        worker_results = [w[0][0] for w in job_manager.run()]
-        val_log_liks = [res.log_lik for res in worker_results]
-
-        val_log_lik = np.sum(val_log_liks)
-        logging.info("all hyperparam split-scores %s", val_log_liks)
-        logging.info("hyperparam sum-split-scores %s", val_log_lik)
-        return val_log_lik
-    else:
-        return -np.inf
-
-
-def _get_one_bcode_stability_score(
-        pen_param_results: List[LikelihoodScorerResult],
-        tree_splits: List[TreeDataSplit],
-        orig_tree: CellLineageTree,
-        max_num_leaves: int = None):
-    """
-    @param weight: weight on the tree stability score (0 to 1)
-    @return stability score -- is the variance of the fitted target lambdas across folds
-    """
-    target_param_ests = []
-    is_stable = True
-    scratch_dir = "_output/scratch"
-    targ_stability_scores = []
     worker_list = []
     for pen_param_res, tree_split in zip(pen_param_results, tree_splits):
-        if pen_param_res is None:
-            logging.info("had trouble training. very instable")
-            is_stable = False
-            break
-
-        param_dict = pen_param_res.model_params_dict
-        target_param_ests.append(np.concatenate([
-            param_dict["target_lams"],
-            param_dict["double_cut_weight"],
-            param_dict["trim_long_factor"]]))
-
-        num_nodes = tree_split.tree.get_num_nodes()
-        num_val_nodes = len(tree_split.val_obs)
+        # Use all the fitted params from the training data since we have the
+        # same tree topology
         fit_params = pen_param_res.get_fit_params()
-        new_br_inners_mask = np.zeros(num_nodes + num_val_nodes, dtype=bool)
-        new_br_inners_mask[:num_nodes] = True
-        new_br_offsets_mask = np.zeros(num_nodes + num_val_nodes, dtype=bool)
-        new_br_offsets_mask[:num_nodes] = True
-        new_br_inners = np.ones(num_nodes + num_val_nodes) * 1e-10
-        new_br_inners[:num_nodes] = fit_params['branch_len_inners']
-        new_br_offsets = np.ones(num_nodes + num_val_nodes) * 0.1
-        new_br_offsets[:num_nodes] = fit_params['branch_len_offsets_proportion']
-        train_val_tree = tree_split.tree.copy()
-        for node in train_val_tree.traverse():
-            if len(node.get_children()) == 2:
-                node.resolved_multifurcation = True
-        for leaf_idx, (orig_leaf, leaf_par_id) in enumerate(tree_split.val_obs):
-            leaf = orig_leaf.copy()
-            leaf.add_feature("node_id", leaf_idx + num_nodes)
-            parent_node = train_val_tree.search_nodes(orig_node_id=leaf_par_id)[0]
-            parent_node.add_child(leaf)
         all_known_params = KnownModelParams(
-                target_lams=True,
-                tot_time=True,
-                indel_params=True,
-                branch_lens=True,
-                branch_len_inners=new_br_inners_mask,
-                branch_len_offsets_proportion=new_br_offsets_mask)
-        fit_params['branch_len_inners'] = new_br_inners
-        fit_params['branch_len_offsets_proportion'] = new_br_offsets
-        fit_params['branch_pen_param'] = 0
-
+            target_lams=True,
+            tot_time=True,
+            indel_params=True)
         transition_wrap_maker = TransitionWrapperMaker(
-            train_val_tree,
-            tree_split.bcode_meta,
-            100,
-            1000)
+            tree_split.val_clt,
+            tree_split.val_bcode_meta,
+            max_extra_steps,
+            max_sum_states)
         scorer = LikelihoodScorer(
-            1, # seed
-            train_val_tree,
-            tree_split.bcode_meta,
-            max_iters=0, #50000,
+            get_randint(),  # seed
+            tree_split.val_clt,
+            tree_split.val_bcode_meta,
+            # No iterations because we just want to evaluate a probability
+            max_iters=0,
             num_inits=1,
             transition_wrap_maker=transition_wrap_maker,
             fit_param_list=[fit_params],
@@ -332,28 +245,91 @@ def _get_one_bcode_stability_score(
     job_manager = SubprocessManager(
             worker_list,
             None,
-            "_output/scratch",
-            10)
+            scratch_dir,
+            num_processes)
     worker_results = [w[0][0] for w in job_manager.run()]
-    #targ_stability_scores = [
-    #        res.log_lik/len(tree_split.val_obs)
-    #        for res, tree_split in zip(
-    #            worker_results,
-    #            tree_splits)]
-    targ_stability_scores = [
+    val_log_liks = [res.log_lik for res in worker_results]
+
+    tot_val_log_lik = np.sum(val_log_liks)
+    logging.info("all hyperparam split-scores %s, (sum %f)", val_log_liks, tot_val_log_lik)
+    return tot_val_log_lik
+
+
+def _get_one_bcode_hyperparam_score(
+        pen_param_results: List[LikelihoodScorerResult],
+        tree_splits: List[TreeDataSplit],
+        max_extra_steps: int,
+        max_sum_states: int,
+        scratch_dir: str,
+        num_processes: int):
+    """
+    @return score = Pr(validation data | train data)
+    """
+    for pen_param_res in pen_param_results:
+        if pen_param_res is None:
+            # This pen param setting is not stable
+            return -np.inf
+
+    worker_list = []
+    for pen_param_res, tree_split in zip(pen_param_results, tree_splits):
+        # Need to create model parameters for the full tree since
+        # we only trained on a subset of the leaves
+
+        # First we need to preserve any bifurcations in the train tree
+        for node in tree_split.train_clt.traverse():
+            if len(node.get_children()) == 2:
+                node.resolved_multifurcation = True
+
+        # Let's start creating the branch lenght assignments for the
+        # validation leaves
+        fit_params = pen_param_res.get_fit_params()
+        num_tot_nodes = tree_split.val_clt.get_num_nodes()
+        num_train_nodes = tree_split.train_clt.get_num_nodes()
+
+        new_br_inners = np.ones(num_tot_nodes) * 1e-10
+        new_br_inners[:num_train_nodes] = fit_params['branch_len_inners']
+        # We will place the validation leaves at the top of the multifurcation
+        # This is a somewhat arbitrary choice.
+        # However we definitely cannot maximize validation log lik wrt the validation offsets.
+        # Otherwise penalty param picking will not work.
+        new_br_offsets = np.ones(num_tot_nodes) * 1e-10
+        new_br_offsets[:num_train_nodes] = fit_params['branch_len_offsets_proportion']
+        fit_params['branch_len_inners'] = new_br_inners
+        fit_params['branch_len_offsets_proportion'] = new_br_offsets
+        all_known_params = KnownModelParams(
+                target_lams=True,
+                tot_time=True,
+                indel_params=True)
+
+        transition_wrap_maker = TransitionWrapperMaker(
+            tree_split.val_clt,
+            tree_split.val_bcode_meta,
+            max_extra_steps,
+            max_sum_states)
+        scorer = LikelihoodScorer(
+            get_randint(),  # seed
+            tree_split.val_clt,
+            tree_split.val_bcode_meta,
+            # No iterations because we just want to evaluate a probability
+            max_iters=0,
+            num_inits=1,
+            transition_wrap_maker=transition_wrap_maker,
+            fit_param_list=[fit_params],
+            known_params=all_known_params,
+            scratch_dir=scratch_dir)
+        worker_list.append(scorer)
+
+    job_manager = SubprocessManager(
+            worker_list,
+            None,
+            scratch_dir,
+            num_processes)
+    worker_results = [w[0][0] for w in job_manager.run()]
+
+    # Get Pr(V|T)
+    hyperparam_scores = [
             res.log_lik - pen_param_res.log_lik
-            for res, pen_param_res in zip(
-                worker_results,
-                pen_param_results)]
-
-    targ_stability_score = np.sum(targ_stability_scores)
-    logging.info("all stability scores %s", targ_stability_scores)
-
-    if is_stable:
-        mean_target_param_est = sum(target_param_ests)/len(target_param_ests)
-        logging.info("mean targ %s", mean_target_param_est)
-        logging.info("stability scores %s", targ_stability_score)
-        stability_score = targ_stability_score
-        return stability_score
-    else:
-        return -np.inf
+            for res, pen_param_res in zip(worker_results, pen_param_results)]
+    tot_hyperparam_score = np.sum(hyperparam_scores)
+    logging.info("all Pr(Val given T) %s (sum %f)", hyperparam_scores, tot_hyperparam_score)
+    return tot_hyperparam_score
--- gestalt/split_data.py
+++ gestalt/split_data.py
@@ -17,30 +17,43 @@ class TreeDataSplit:
     """
     Stores the metadata and data for each "fold" from our variant of k-fold CV
     """
-    def __init__(self,
-            tree: CellLineageTree,
-            val_obs: List[CellLineageTree],
-            bcode_meta: BarcodeMetadata,
-            train_bcode_idxs: List[int],
-            val_bcode_idxs: List[int],
-            node_to_orig_id: Dict[int, int],
-            val_clt: CellLineageTree = None,
-            val_bcode_meta: BarcodeMetadata = None,
-            is_kfold_tree: bool = False):
+    def __init__(
+            self,
+            train_clt: CellLineageTree,
+            train_bcode_meta: BarcodeMetadata,
+            val_clt: CellLineageTree,
+            val_bcode_meta: BarcodeMetadata):
         """
         @param is_kfold_tree: True = we split the tree into subtrees (for the 1 bcode case)
         """
-        self.tree = tree
-        self.val_obs = val_obs
+        self.train_clt = train_clt
+        self.train_bcode_meta = train_bcode_meta
         self.val_clt = val_clt
-        self.bcode_meta = bcode_meta
-        self.train_bcode_idxs = train_bcode_idxs
-        self.val_bcode_idxs = val_bcode_idxs
         self.val_bcode_meta = val_bcode_meta
-        self.node_to_orig_id = node_to_orig_id
-        self.is_kfold_tree = is_kfold_tree
 
-def create_kfold_trees(tree: CellLineageTree, bcode_meta: BarcodeMetadata, n_splits: int):
+
+def _pick_random_validation_node_ids(tree: CellLineageTree, min_multifurc_children: int):
+    val_obs = []
+    for node in tree.traverse():
+        if node.is_leaf():
+            continue
+
+        if len(node.get_children()) <= min_multifurc_children:
+            # Only create validation leaves from multifurcations with
+            # 4 or more children
+            continue
+
+        leaf_children = [c for c in node.get_children() if c.is_leaf()]
+        leaf = random.choice(leaf_children)
+        val_obs.append((leaf.copy(), leaf.up.node_id))
+    return val_obs
+
+
+def create_kfold_trees(
+        tree: CellLineageTree,
+        bcode_meta: BarcodeMetadata,
+        n_splits: int,
+        min_multifurc_children: int = 3):
     """
     Take a tree and create k-fold datasets based on children of the root node
     This takes a tree and creates subtrees by taking a subset of the leaves,
@@ -52,52 +65,44 @@ def create_kfold_trees(tree: CellLineageTree, bcode_meta: BarcodeMetadata, n_spl
     """
     all_train_trees = []
     for i in range(n_splits):
-        print("split")
         tree_copy = tree.copy()
 
-        val_obs = []
-        for node in tree_copy.traverse():
-            if node.is_leaf():
-                continue
-
-            if len(node.get_children()) <= 3:
-                continue
-
-            leaf_children = [c for c in node.get_children() if c.is_leaf()]
-            leaf = random.choice(leaf_children)
-            val_obs.append((
-                leaf.copy(),
-                leaf.up.node_id))
-            logging.info('val obs %d', leaf.node_id)
-            print("leaf id", leaf.node_id, "parent node id", leaf.up.node_id)
-
+        val_obs = _pick_random_validation_node_ids(tree_copy, min_multifurc_children)
         val_obs_ids = set([n.node_id for n, _ in val_obs])
+        if len(val_obs) == 0:
+            # If cannot find enough validation leaves, try a smaller threshold
+            val_obs = _pick_random_validation_node_ids(tree_copy, max(min_multifurc_children - 1, 3))
+            val_obs_ids = set([n.node_id for n, _ in val_obs])
+        assert len(val_obs) > 0
+
+        # Prune tree to create our tree for training
         train_leaf_ids = set()
         for leaf in tree_copy:
             if leaf.node_id not in val_obs_ids:
                 train_leaf_ids.add(leaf.node_id)
-
         train_tree = CellLineageTree.prune_tree(tree_copy, train_leaf_ids)
-        logging.info("before")
-        logging.info("SAMPLED TREE")
-        logging.info(train_tree.get_ascii(attributes=["node_id"], show_internal=True))
 
         for node in train_tree.traverse():
             node.add_feature("orig_node_id", node.node_id)
-        train_tree.label_node_ids()
+        num_train_tree_nodes = train_tree.label_node_ids()
 
-        node_to_orig_id = dict()
+        # Create the validation tree -- just attach back on our validation leaves
+        val_tree = train_tree.copy()
+        for leaf_idx, (orig_leaf, leaf_par_id) in enumerate(val_obs):
+            leaf = orig_leaf.copy()
+            leaf.add_feature("node_id", leaf_idx + num_train_tree_nodes)
+            parent_node = val_tree.search_nodes(orig_node_id=leaf_par_id)[0]
+            parent_node.add_child(leaf)
+
+        # Clean up the extra attribute
         for node in train_tree.traverse():
-            node_to_orig_id[node.node_id] = node.orig_node_id
+            node.del_feature("orig_node_id")
 
         all_train_trees.append(TreeDataSplit(
             train_tree,
-            val_obs,
             bcode_meta,
-            [0],
-            [0],
-            node_to_orig_id,
-            is_kfold_tree=True))
+            val_tree,
+            bcode_meta))
 
     return all_train_trees
 
@@ -137,13 +142,8 @@ def create_kfold_barcode_trees(tree: CellLineageTree, bcode_meta: BarcodeMetadat
         val_clt.restrict_barcodes(val_idxs)
         all_train_trees.append(TreeDataSplit(
             train_clt,
-            [],
             train_bcode_meta,
-            train_bcode_idxs=bcode_idxs,
-            val_bcode_idxs=bcode_idxs,
-            node_to_orig_id={},
             val_clt=val_clt,
-            val_bcode_meta=val_bcode_meta,
-            is_kfold_tree=False))
+            val_bcode_meta=val_bcode_meta))
 
     return all_train_trees
