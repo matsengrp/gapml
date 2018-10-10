6007cbb03131cd373ddde355aecb3697627a23b5
Jean Feng <jean.feng@gmail.com>
2018-10-09 17:15:14 -0700

    update hypothesis test for cell types

--- gestalt/plot_analyze_gestalt_btw_fish.py
+++ gestalt/plot_analyze_gestalt_btw_fish.py
@@ -3,6 +3,7 @@ import sys
 import argparse
 import os.path
 import numpy as np
+import pandas as pd
 import scipy.stats
 
 import matplotlib
--- gestalt/plot_analyze_gestalt_cell_types.py
+++ gestalt/plot_analyze_gestalt_cell_types.py
@@ -5,10 +5,11 @@ matplotlib.use('Agg')
 from matplotlib import pyplot
 import seaborn as sns
 import numpy as np
+import scipy.stats
 import pandas as pd
 from scipy import stats
 
-from plot_analyze_gestalt_meta import load_fish, get_allele_to_cell_states
+from plot_analyze_gestalt_meta import load_fish, get_allele_to_cell_states, create_shuffled_cell_state_abund_labels
 
 ECTO = 0
 NEURAL_CREST = 1
@@ -32,6 +33,10 @@ def parse_args(args):
     parser = argparse.ArgumentParser(
             description='plot time in tree vs number of descendant cell types/germ layers')
     parser.add_argument(
+        '--num-rands',
+        type=int,
+        default=1000)
+    parser.add_argument(
         '--out-germ-layer-plot-template',
         type=str,
         default="_output/time_to_germ_layer_%s.png")
@@ -42,17 +47,15 @@ def parse_args(args):
     args = parser.parse_args(args)
     return args
 
-def plot_distance_to_num_germ_layers(
+def get_distance_to_num_germ_layers(
         fitted_bifurc_tree,
         allele_to_cell_state,
-        organ_dict,
-        out_plot_file):
+        organ_dict):
     """
     """
     X_dists = []
     Y_n_germ_layers = []
     for node in fitted_bifurc_tree.traverse('postorder'):
-        dist = node.get_distance(fitted_bifurc_tree)
         if node.is_leaf():
             allele_str = node.allele_events_list_str
             germ_layers = list(allele_to_cell_state[allele_str].keys())
@@ -68,11 +71,15 @@ def plot_distance_to_num_germ_layers(
             continue
         if min([len(leaf.germ_layers) for leaf in node]) > 1:
             continue
-        dist = node.get_distance(fitted_bifurc_tree)
-        X_dists.append(dist)
+        X_dists.append(node.dist_to_root)
         n_germ_layers = len(node.germ_layers)
         Y_n_germ_layers.append(n_germ_layers)
+    return X_dists, Y_n_germ_layers
 
+def plot_distance_to_num_germ_layers(
+        X_dists,
+        Y_n_germ_layers,
+        out_plot_file):
     print(out_plot_file)
     pyplot.clf()
     data = pd.DataFrame.from_dict({
@@ -91,19 +98,12 @@ def plot_distance_to_num_germ_layers(
     pyplot.ylabel("Number of germ layers")
     pyplot.xlabel("Distance from root")
     pyplot.savefig(out_plot_file)
-    print(stats.linregress(X_dists, Y_n_germ_layers))
 
-def plot_distance_to_num_cell_states(
+def get_distance_to_num_cell_states(
         fitted_bifurc_tree,
         allele_to_cell_state,
-        organ_dict,
-        out_plot_file):
-    """
-    Understand if distance to root is inversely related to number of
-    different cell states in leaves
-    """
+        organ_dict):
     for node in fitted_bifurc_tree.traverse('postorder'):
-        dist = node.get_distance(fitted_bifurc_tree)
         if node.is_leaf():
             allele_str = node.allele_events_list_str
             node.add_feature("cell_types", set(list(allele_to_cell_state[allele_str].keys())))
@@ -119,10 +119,18 @@ def plot_distance_to_num_cell_states(
             continue
         if min([len(leaf.cell_types) for leaf in node]) > 1:
             continue
-        dist = node.get_distance(fitted_bifurc_tree)
-        X_dists.append(dist)
+        X_dists.append(node.dist_to_root)
         Y_n_cell_states.append(len(node.cell_types))
+    return X_dists, Y_n_cell_states
 
+def plot_distance_to_num_cell_states(
+        X_dists,
+        Y_n_cell_states,
+        out_plot_file):
+    """
+    Understand if distance to root is inversely related to number of
+    different cell states in leaves
+    """
     print(out_plot_file)
     pyplot.clf()
     data = pd.DataFrame.from_dict({
@@ -141,7 +149,6 @@ def plot_distance_to_num_cell_states(
     pyplot.ylabel("Number of descendant cell types")
     pyplot.xlabel("Distance from root")
     pyplot.savefig(out_plot_file)
-    print(stats.linregress(X_dists, Y_n_cell_states))
 
 
 def plot_branch_len_time(
@@ -172,26 +179,72 @@ def plot_branch_len_time(
     print("branch len vs node time", stats.linregress(X_dist, Y_branch_len))
     print("leaf branch lens mean", np.mean(leaf_branch_lens), np.min(leaf_branch_lens), np.max(leaf_branch_lens))
 
+def do_hypothesis_test(
+        estimated_X_Y,
+        tree,
+        allele_to_cell_state,
+        organ_dict,
+        get_X_Y_func,
+        num_rands):
+    """
+    Formally, the null hypothesis is the cell types and abundances are completely
+    randomly assigned to the leaves of the two trees.
+    """
+    corr, stupid_pval = scipy.stats.pearsonr(estimated_X_Y[0], estimated_X_Y[1])
+    all_rand_corrs = []
+    for _ in range(num_rands):
+        shuffled_meta = create_shuffled_cell_state_abund_labels(allele_to_cell_state)
+        rand_X, rand_Y = get_X_Y_func(tree, shuffled_meta, organ_dict)
+        rand_corr, _ = scipy.stats.pearsonr(rand_X, rand_Y)
+        all_rand_corrs.append(rand_corr)
+    print('correlation', corr, 'stupid pval', stupid_pval)
+    print("random mean corr", np.mean(all_rand_corrs))
+    print('p-val', np.mean(np.abs(corr) < np.abs(all_rand_corrs)))
+
 def main(args=sys.argv[1:]):
     args = parse_args(args)
     fishies = ["ADR1", "ADR2"]
-    do_chronoses = [True, False]
-    for do_chronos in do_chronoses:
+    methods = ["PMLE", "chronos", "nj"]
+    for method in methods:
+        print("METHOD", method)
         for fish in fishies:
-            tree, obs_dict = load_fish(fish, do_chronos=do_chronos)
+            print("FISH", fish)
+            tree, obs_dict = load_fish(fish, method)
+            tree.label_dist_to_roots()
             allele_to_cell_state, cell_state_dict = get_allele_to_cell_states(obs_dict)
             organ_dict = obs_dict["organ_dict"]
 
+            X_dists, Y_n_germ_layers = get_distance_to_num_germ_layers(
+                tree,
+                allele_to_cell_state,
+                organ_dict)
+            do_hypothesis_test(
+                (X_dists, Y_n_germ_layers),
+                tree,
+                allele_to_cell_state,
+                organ_dict,
+                get_distance_to_num_germ_layers,
+                args.num_rands)
             plot_distance_to_num_germ_layers(
+                X_dists,
+                Y_n_germ_layers,
+                args.out_germ_layer_plot_template % fish)
+
+            X_dists, Y_n_cell_states = get_distance_to_num_cell_states(
+                tree,
+                allele_to_cell_state,
+                organ_dict)
+            do_hypothesis_test(
+                (X_dists, Y_n_cell_states),
                 tree,
                 allele_to_cell_state,
                 organ_dict,
-                out_plot_file = args.out_germ_layer_plot_template % fish)
+                get_distance_to_num_cell_states,
+                args.num_rands)
             plot_distance_to_num_cell_states(
-                tree,
-                allele_to_cell_state,
-                organ_dict,
-                out_plot_file = args.out_cell_type_plot_template % fish)
+                X_dists,
+                Y_n_cell_states,
+                args.out_cell_type_plot_template % fish)
     #print("plot branch length distribution")
     #plot_branch_len_time(
     #    res.fitted_bifurc_tree,
