69d5ca049952362457783f8e37ba403badb52153
Jean Feng <jean.feng@gmail.com>
2020-02-03 08:51:39 -0800

    fix tests

--- gestalt/tests/test_allele_simulator.py
+++ gestalt/tests/test_allele_simulator.py
@@ -31,7 +31,6 @@ class AlleleSimulatorTestCase(unittest.TestCase):
         self.topology.add_child(self.node)
         self.topology.label_dist_to_roots()
 
-    @unittest.skip("Asdf")
     def test_race_process_no_events(self):
         mdl = CLTLikelihoodModel(
                 None,
@@ -51,14 +50,14 @@ class AlleleSimulatorTestCase(unittest.TestCase):
         self.assertEqual(race_winner, None)
         self.assertEqual(event_time, None)
 
-    @unittest.skip("Asdf")
     def test_simulate(self):
         mdl = CLTLikelihoodModel(
                 None,
                 self.bcode_meta,
                 self.sess,
                 known_params = self.known_params,
-                target_lams = 1 + np.arange(self.bcode_meta.n_targets))
+                target_lams = 1 + np.arange(self.bcode_meta.n_targets),
+                use_poisson=False)
         self._create_simulator(mdl)
         old_allele_str = str(self.allele)
 
@@ -76,104 +75,10 @@ class AlleleSimulatorTestCase(unittest.TestCase):
         # Allele should be contiguous
         self.assertEqual(new_allele.get_target_status(), TargetStatus(TargetDeactTract(0,9)))
 
-    @unittest.skip("Asdf")
-    def test_neg_beta_insert_focal(self):
-        insert_poiss = np.array([1])
-        insert_zero_prob = np.array([0.1])
-        boost_softmax_weights = np.array([1,1,1])
-        mdl = CLTLikelihoodModel(
-                None,
-                self.bcode_meta,
-                self.sess,
-                known_params = self.known_params,
-                target_lams = 1 + np.arange(self.bcode_meta.n_targets),
-                boost_softmax_weights = boost_softmax_weights,
-                insert_poiss = insert_poiss,
-                insert_zero_prob = insert_zero_prob)
-        self._create_simulator(mdl)
-
-        target_tract = TargetTract(0,0,0,0)
-        num_replicates = 8000
-        left_trims = []
-        right_trims = []
-        insert_lens = []
-        for i in range(num_replicates):
-            allele = self.allele_sim.get_root().alleles[0]
-            left_trim_raw, right_trim_raw, insert_str_raw = self.allele_sim._do_repair(allele, target_tract)
-            allele_events = allele.get_event_encoding()
-            evt = allele_events.events[0]
-            left_trim, right_trim = evt.get_trim_lens(self.bcode_meta)
-            insert_len = evt.insert_len
-            assert right_trim == right_trim_raw
-            assert left_trim == left_trim_raw
-            assert insert_len == len(insert_str_raw)
-            right_trims.append(right_trim)
-            left_trims.append(left_trim)
-            insert_lens.append(insert_len)
-
-        mean_nbinom = insert_poiss
-        var_nbinom = insert_poiss
-        second_moment_nbinom = var_nbinom + np.power(mean_nbinom, 2)
-        insert_mean_true = 1./3 * (1 + mean_nbinom) + 2./3 * (1 - insert_zero_prob) * mean_nbinom
-        insert_var_true = 1/3. * 2/3. + (1 - insert_zero_prob) * second_moment_nbinom - np.power((1 - insert_zero_prob) * mean_nbinom, 2)
-
-        print(np.mean(insert_lens), np.var(insert_lens))
-        print(insert_mean_true, insert_var_true)
-        self.assertTrue(np.mean(insert_lens) < insert_mean_true + 2 * np.sqrt(insert_var_true/num_replicates))
-        self.assertTrue(insert_mean_true - 2 * np.sqrt(insert_var_true/num_replicates) < np.mean(insert_lens))
-        self.assertTrue(np.isclose(np.var(insert_lens), insert_var_true, atol=0.2))
-
-    @unittest.skip("Asdf")
-    def test_neg_beta_insert_intertarg(self):
-        insert_poiss = np.array([1])
-        insert_zero_prob = np.array([0.1])
-        boost_softmax_weights = np.array([1,1,1])
-        mdl = CLTLikelihoodModel(
-                None,
-                self.bcode_meta,
-                self.sess,
-                known_params = self.known_params,
-                target_lams = 1 + np.arange(self.bcode_meta.n_targets),
-                boost_softmax_weights = boost_softmax_weights,
-                insert_poiss = insert_poiss,
-                insert_zero_prob = insert_zero_prob)
-        self._create_simulator(mdl)
-
-        target_tract = TargetTract(0,0,1,1)
-        num_replicates = 8000
-        left_trims = []
-        right_trims = []
-        insert_lens = []
-        for i in range(num_replicates):
-            allele = self.allele_sim.get_root().alleles[0]
-            left_trim_raw, right_trim_raw, insert_str_raw = self.allele_sim._do_repair(allele, target_tract)
-            allele_events = allele.get_event_encoding()
-            evt = allele_events.events[0]
-            left_trim, right_trim = evt.get_trim_lens(self.bcode_meta)
-            insert_len = evt.insert_len
-            assert right_trim == right_trim_raw
-            assert left_trim == left_trim_raw
-            assert insert_len == len(insert_str_raw)
-            right_trims.append(right_trim)
-            left_trims.append(left_trim)
-            insert_lens.append(insert_len)
-
-        mean_nbinom = insert_poiss
-        var_nbinom = insert_poiss
-        second_moment_nbinom = var_nbinom + np.power(mean_nbinom, 2)
-        insert_mean_true = (1 - insert_zero_prob) * mean_nbinom
-        insert_var_true = (1 - insert_zero_prob) * second_moment_nbinom - np.power((1 - insert_zero_prob) * mean_nbinom, 2)
-
-        print(np.mean(insert_lens), np.var(insert_lens))
-        print(insert_mean_true, insert_var_true)
-        self.assertTrue(np.mean(insert_lens) < insert_mean_true + 2 * np.sqrt(insert_var_true/num_replicates))
-        self.assertTrue(insert_mean_true - 2 * np.sqrt(insert_var_true/num_replicates) < np.mean(insert_lens))
-        self.assertTrue(np.isclose(np.var(insert_lens), insert_var_true, atol=0.1))
-
     def test_neg_beta_right_del_focal(self):
         trim_zero_probs = np.array([0.1,0.2,0.4,0.2])
-        trim_short_params = np.array([1,0.3,1,0.1,2,-1,1,-0.2])
-        trim_short_params_reshape = trim_short_params.reshape([4,-1])
+        trim_short_params = np.array([1,0.3,1,0.1])
+        trim_short_params_reshape = trim_short_params.reshape([2,-1])
         boost_weight = np.array([1,2,2])
         boost_prob = np.exp(boost_weight)/np.sum(np.exp(boost_weight))
         print(trim_short_params_reshape)
@@ -208,7 +113,7 @@ class AlleleSimulatorTestCase(unittest.TestCase):
             left_trims.append(left_trim)
             insert_lens.append(insert_len)
 
-        dist_index = 2
+        dist_index = 1
         trim_right_zero_prob = trim_zero_probs[dist_index]
         max_trim_len = self.bcode_meta.right_long_trim_min[1] - 1
         trim_right_dist = PaddedBoundedNegativeBinomial(
@@ -226,60 +131,3 @@ class AlleleSimulatorTestCase(unittest.TestCase):
             + (1 - boost_prob[2]) * (1 - trim_right_zero_prob) * np.sum([k * trim_right_dist.pmf(k) for k in range(1, max_trim_len + 1)]))
 
         self.assertTrue(np.isclose(np.mean(right_trims), right_trim_mean_true, atol=0.1))
-
-    @unittest.skip("asdf")
-    def test_neg_beta_left_del_intertarg(self):
-        trim_zero_probs = np.array([0.1,0.2,0.4,0.2])
-        trim_short_params = np.array([1,0.3,1,0.1,2,0.5,1,-0.2])
-        trim_short_params_reshape = trim_short_params.reshape([4,-1])
-        print(trim_short_params_reshape)
-        mdl = CLTLikelihoodModel(
-                None,
-                self.bcode_meta,
-                self.sess,
-                known_params = self.known_params,
-                target_lams = 1 + np.arange(self.bcode_meta.n_targets),
-                trim_zero_probs = trim_zero_probs,
-                trim_short_params = trim_short_params,
-                use_poisson = False)
-        self._create_simulator(mdl)
-
-        target_tract = TargetTract(0,0,1,1)
-        num_replicates = 8000
-        left_trims = []
-        right_trims = []
-        insert_lens = []
-        for i in range(num_replicates):
-            allele = self.allele_sim.get_root().alleles[0]
-            left_trim_raw, right_trim_raw, insert_str_raw = self.allele_sim._do_repair(allele, target_tract)
-            allele_events = allele.get_event_encoding()
-            evt = allele_events.events[0]
-            left_trim, right_trim = evt.get_trim_lens(self.bcode_meta)
-            insert_len = evt.insert_len
-            assert right_trim == right_trim_raw
-            assert left_trim == left_trim_raw
-            assert insert_len == len(insert_str_raw)
-            right_trims.append(right_trim)
-            left_trims.append(left_trim)
-            insert_lens.append(insert_len)
-
-        dist_index = 1
-        trim_left_zero_prob = trim_zero_probs[dist_index]
-        max_trim_len = self.bcode_meta.left_long_trim_min[0] - 1
-        trim_left_dist = PaddedBoundedNegativeBinomial(
-                0,
-                max_trim_len,
-                np.exp(trim_short_params_reshape[dist_index,0]),
-                trim_short_params_reshape[dist_index,1])
-        mean_nbinom = np.sum([
-            k * trim_left_dist.pmf(k) for k in range(max_trim_len + 1)])
-        left_trim_mean_true = (1 - trim_left_zero_prob) * mean_nbinom
-        second_moment_nbinom = np.sum([
-            np.power(k, 2) * trim_left_dist.pmf(k) for k in range(max_trim_len + 1)])
-        left_trim_var_true = (1 - trim_left_zero_prob) * second_moment_nbinom - np.power(mean_nbinom * (1 - trim_left_zero_prob), 2)
-
-        print(np.mean(left_trims), np.var(left_trims))
-        print(left_trim_mean_true, left_trim_var_true)
-        self.assertTrue(np.mean(left_trims) < left_trim_mean_true + 2 * np.sqrt(left_trim_var_true/num_replicates))
-        self.assertTrue(left_trim_mean_true - 2 * np.sqrt(left_trim_var_true/num_replicates) < np.mean(left_trims))
-        self.assertTrue(np.isclose(left_trim_var_true, np.var(left_trims)))
